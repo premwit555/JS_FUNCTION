093a892610c08b44479f86175d67c9be6dd8a67e
min-macbook <premwit.min@gmail.com>
2021-10-09 16:32:49 +0700

    deffDate

--- date/index.js
+++ date/index.js
@@ -3,14 +3,21 @@ import moment from "moment"
 const now = moment()
 const Now = moment().format()
 
-
-const L = moment().format('L')
-const Lh = moment(1633729483).format('L-HH:mm:ss')   //set timne
-const Lnow = moment().format('L-HH:mm:ss') 				// now time
-
+const L = moment().format("L")
+const Lh = moment(1633729483).format("L-HH:mm:ss") //set timne
+const Lnow = moment().format("L-HH:mm:ss") // now time
 
 const cNow = moment().format("X")
 
+const time = {
+     min: 60 * 60 * 60,
+     min5: 60 * 60 * 60 * 5,
+     min10: 60 * 60 * 60 * 10,
+	 hour: 60 * 60 * 60 *60,
+	 hour6: 60 * 60 * 60 *60*6,
+	 hour12: 60 * 60 * 60 *60*12,
+	 day: 60 * 60 * 60 *60*24,
+}
 
 
 // console.log(Lh)
@@ -23,21 +30,16 @@ const cNow = moment().format("X")
 const c = moment("2021-10-09T04:51:09+07:00")
 // console.log(c);
 
+const deff = c.diff(now)
 
-const deff =c.diff(now,)
+// function find
+const varTime = "2021-10-09T04:51:09+07:00"
+const countdown = time.min5
 
-const deffomrmat = moment("2021-10-09T04:51:09+07:00")
-
-// 
-console.log(60*60*60*5);
-// 216000 = 1 min
-// 1080000 = 5min
-
-
-const time =now.diff(deffomrmat)
+const lastTime = now.diff(moment(varTime))
 
 // มีค่า มากกว่า 10 นาที
-if(time<(60*60*60*10)){
-	console.log(time);
+if (lastTime > countdown) {
+     // if time > min5 start function
+     console.log(Now)
 }
-
